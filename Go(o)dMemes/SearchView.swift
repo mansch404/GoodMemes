9920969167fd98559c41720844e1e6a62f437cbb
mansch404 <manuel-asbeck5@t-online.de>
2021-10-31 21:00:18 +0100

    Updated SearchView

--- Go(o)dMemes/SearchView.swift
+++ Go(o)dMemes/SearchView.swift
@@ -12,14 +12,8 @@ struct SearchView: View {
     var body: some View {
         NavigationView {
             VStack {
-                ForEach(0..<5) { _ in
-                HStack {
-                    ForEach(0..<3) { x in
-                        MemeTabView(showsMenuTab: false)
-                            .frame(width: UIScreen.main.bounds.width / 3.1, height: UIScreen.main.bounds.width / 3.1)
-                    }
-                }
-            }
+                
+            
             }.padding(.top)
             
                 .navigationTitle("Search for Memes")
